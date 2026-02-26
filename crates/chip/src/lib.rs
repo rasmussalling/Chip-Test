eddf8e2d0ba01b90a086810c2d82788255d8e71d
rasmussalling <s224788@dtu.dk>
2026-02-26 10:58:32 +0100

    Add variance to post condition

--- crates/chip/src/lib.rs
+++ crates/chip/src/lib.rs
@@ -60,6 +60,62 @@ pub fn generate_sample_program() -> String {
 
     let mut values: HashMap<Variable, Vec<i32>> = HashMap::new();
 
+    for _ in 0..10 {
+        let mem = InterpreterMemory {
+            variables: [
+                (Variable("a".to_string()), rng.random_range(-10..10)),
+                (Variable("b".to_string()), rng.random_range(-10..10)),
+                (Variable("c".to_string()), rng.random_range(-10..10)),
+            ]
+            .into_iter()
+            .collect(),
+            arrays: Default::default(),
+        };
+
+        let mut exe = Execution::new(mem.clone());
+
+        for _ in 0..trace_length {
+            if let Some(next) = exe.nexts(&pg).first().cloned() {
+                if next.is_stuck(&pg) {
+                    exe = next;
+                    break;
+                }
+                exe = next;
+                continue;
+            }
+
+            break;
+        }
+
+        for (var, val) in exe.current_mem().variables.iter() {
+            values.entry(var.clone()).or_default().push(*val);
+        }
+    }
+
+    let rand_a = rng.random_range(0..9);
+    let mut gr_we_a = ">=";
+    if rand_a >= 5 {
+        gr_we_a = "<=";
+    }
+    let post_a = format!("a {} {}", gr_we_a, values.get(&Variable("a".into())).and_then(|vals| vals.get(rand_a)).unwrap_or(&0));
+
+    let rand_b = rng.random_range(0..9);
+    let mut gr_we_b = ">=";
+    if rand_b >= 5 {
+        gr_we_b = "<=";
+    }
+    let post_b = format!("b {} {}", gr_we_b, values.get(&Variable("b".into())).and_then(|vals| vals.get(rand_b)).unwrap_or(&0));
+
+    let rand_c = rng.random_range(0..9);
+    let mut gr_we_c = ">=";
+    if rand_c >= 5 {
+        gr_we_c = "<=";
+    }
+    let post_c = format!("c {} {}", gr_we_c, values.get(&Variable("c".into())).and_then(|vals| vals.get(rand_c)).unwrap_or(&0));
+
+    let post_cond = format!("{{{}}}", vec![post_a, post_b, post_c].join(" & "));
+
+    /*
     let mut mem = InterpreterMemory {
             variables: [
                 (Variable("a".to_string()), rng.random_range(-10..10)),
@@ -71,7 +127,7 @@ pub fn generate_sample_program() -> String {
             arrays: Default::default(),
     };
 
-    let start = mem.variables.iter().map(|(v, val)| format!("{} = {}", v, val)).join(" & ");
+    //let start = mem.variables.iter().map(|(v, val)| format!("{} = {}", v, val)).join(" & ");
 
     let mut exe = Execution::new(mem.clone());
 
@@ -88,7 +144,8 @@ pub fn generate_sample_program() -> String {
         break;
     }
 
-    let result = exe.current_mem().variables.iter().map(|(v, val)| format!("{} = {}", v, val)).join(" & ");
-    
-    format!("{{{}}}\n\n{}\n\n{{{}}}",start, string_program, result)
+    let result: String = exe.current_mem().variables.iter().map(|(v, val)| format!("{} = {}", v, val)).join(" & ");
+    */
+
+    format!("{}\n\n{}", string_program, post_cond)
 }
